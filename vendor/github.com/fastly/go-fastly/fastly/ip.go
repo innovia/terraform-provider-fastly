46ed70c0059a49fe855fa9653f4f62d8a92af945
Ami Mahloof <ami@doit-intl.com>
2020-08-31 17:14:40 -0700

    wip

--- vendor/github.com/fastly/go-fastly/fastly/ip.go
+++ vendor/github.com/fastly/go-fastly/fastly/ip.go
@@ -1,5 +1,7 @@
 package fastly
 
+// import "fmt"
+
 // IPAddrs is a sortable list of IP addresses returned by the Fastly API.
 type IPAddrs []string
 
@@ -16,6 +18,16 @@ func (c *Client) AllIPs() (v4, v6 IPAddrs, err error) {
 	}
 
 	return m["addresses"], m["ipv6_addresses"], nil
+}
+
+// ListEntries  list all ip entries on the ACL
+func (c *Client) ListEntries(serviceID, acl string) (v4, v6 IPAddrs, err error) {
+	return []string{"10.0.0.0/32"}, nil, nil
+	// url = fmt.Sprintf("/service/%s/acl/%s/entries", serviceID, acl)
+	// resp, err := c.Get(url)
+	// if err != nil {
+	// 	return nil, nil, fmt.Errorf("error getting entries on service %q from acl %q: %v", serviceID, acl, err)
+	// }
 }
 
 // IPs returns the list of public IPv4 addresses for Fastly's network.
